a757a3d35f6ad2662934b94e2fbe9694dafe18cb
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-01-20 16:51:07 +0100

    trying adding data to the DB

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import DAO.EventiDAO;
+import entities.Evento;
+import entities.tipoEvento;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("U4W3D2_pu");
+        EntityManager em = emf.createEntityManager();
+
+        EventiDAO ed = new EventiDAO(em);
+        Evento concerto = new Evento("Schubert live", "concerto di musica classica", tipoEvento.PUBBLICO, 500);
+        Evento esibizione = new Evento("Echo", "spettacolo del Cirque du Soleil", tipoEvento.PUBBLICO, 600);
+        Evento mostra = new Evento("La seduzione della pittura", "Giovanni Boldini alla Cavallerizza di Piazzale verdi di Lucca", tipoEvento.PUBBLICO, 1000);
+
+        System.out.println(concerto.toString());
+        System.out.println(esibizione.toString());
+        System.out.println(mostra.toString());
+        em.close();
+        emf.close();
     }
 }
